326c72ccbe3cf33c912b2361d9c1968906f14375
elsiossanchez <elsiossanches@gmail.com>
2021-05-13 08:43:08 -0400

    add document to common

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -43,7 +43,7 @@ module.exports = {
               collapsable: false,
               children: [
                 'default-modules/adempiere-api/api',
-                'default-modules/adempiere-api/core',
+                'default-modules/adempiere-api/common',
                 'default-modules/adempiere-api/img',
                 'default-modules/adempiere-api/user',
                 'default-modules/adempiere-api/user-interface',
@@ -110,7 +110,7 @@ module.exports = {
               collapsable: true,
               children: [
                 'default-modules/adempiere-api/api',
-                'default-modules/adempiere-api/core',
+                'default-modules/adempiere-api/common',
                 'default-modules/adempiere-api/img',
                 'default-modules/adempiere-api/user',
                 'default-modules/adempiere-api/user-interface',
